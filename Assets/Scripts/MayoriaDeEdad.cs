ce65fb89407701fd7aa5177858c3adf75cb83e49
benjavitorgan <vitorganbenjamin@gmail.com>
2022-04-01 12:06:03 -0300

    Script if mayor de edad

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int EdadUsuario;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EdadUsuario = 19;
+
+        if (EdadUsuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+        } else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
