29e093b3831586691dd2baa947a208b2421614bb
benjavitorgan <vitorganbenjamin@gmail.com>
2022-04-03 18:02:54 -0300

    Tp-02 completed

---
+++ TP-2---MI3D-main/Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int EdadUsuario;
+    public Text MiTexto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        if (EdadUsuario >= 18)
+        {
+            MiTexto.text = "Es mayor de edad";
+
+            Debug.Log("Es mayor de edad");
+        } else
+        {
+            MiTexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TP-2---MI3D-main/Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string ContraseniaCorrecta;
+    string ContraseniaUsuario;
+    public Text IngresoUsuario;
+    public Text textomsj;
+    public GameObject cartelitoMsj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ContraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ValidarContrasenia()
+    {
+        ContraseniaUsuario = IngresoUsuario.text;
+
+        if (ContraseniaUsuario == ContraseniaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textomsj.text = "Bienvenido";
+            Debug.Log ("Bienvenido");
+        } else
+        {
+            cartelitoMsj.SetActive(true);
+            textomsj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
+ 
